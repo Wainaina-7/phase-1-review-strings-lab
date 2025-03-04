1107c152bdac52b4b1a304f24f5cc500dfcc0fef
Peter kanyori <peterynaenah23@gmail.com>
2025-03-04 19:04:51 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'Peter Wainaina'
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+ let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+// Example of constructing shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
